432729d31ae58e35ba83cb19c5211254c6876fd8
estebanr23 <83772778+estebanr23@users.noreply.github.com>
2022-06-02 12:55:46 -0300

    Algoritmo Prioridad No Apropiativo

--- index.js
+++ index.js
@@ -85,7 +85,7 @@
         */
         
         function crearDiagrama() {
-            secuencia = prioridadApropiativo(procesos); // Cambiamos el nombre para probar algoritmos.
+            secuencia = prioridadNoApropiativo(procesos); // Cambiamos el nombre para probar algoritmos.
 
             t = secuencia.length;
         
