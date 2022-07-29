d7be314aa27ee9e0e8da9a3a44e4a30c9808e591
Esteban Robert <83772778+estebanr23@users.noreply.github.com>
2022-07-29 18:31:54 -0300

    Haciendo el sitio responsive

--- js/menu.js
+++ js/menu.js
@@ -6,5 +6,26 @@
         var subMenu = document.getElementById('submenu');
         enlacePrior.addEventListener('mouseenter', () => subMenu.style.display='block');
         enlacePrior.addEventListener('mouseleave', () => subMenu.style.display='none');
+
+        // menu movil
+        var menuMovil = document.getElementsByClassName('menu-movil')[0];
+        var nav = document.getElementsByTagName('nav')[0];
+        let estado=0;
+
+        menuMovil.addEventListener('click', function() {
+            if(estado === 0) {
+                nav.style.display='block';
+                estado = 1;
+            } else if(estado === 1){
+                nav.style.display='none';
+                estado = 0;
+            }
+        });
+
+        // Año footer
+        var anio = document.getElementById('date');
+        var tiempo = new Date();
+        var year = tiempo.getFullYear();
+        anio.innerHTML=year;
     });
 })();
